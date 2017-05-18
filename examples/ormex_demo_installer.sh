6f724fec437087c83560c297d4fb315f959a722d
Artur Barseghyan <artur.barseghyan@gmail.com>
2017-05-18 02:25:46 +0200

    fixes in demo script

--- examples/ormex_demo_installer.sh
+++ examples/ormex_demo_installer.sh
@@ -1,11 +1,11 @@
-wget -O django_dummy_thumbnails_demo_installer.tar.gz https://github.com/barseghyanartur/django-dummy-thumbnails/archive/stable.tar.gz
-virtualenv django-dummy-thumbnails
-source django-dummy-thumbnails/bin/activate
-mkdir django_dummy_thumbnails_demo_installer/
-tar -xvf django_dummy_thumbnails_demo_installer.tar.gz -C django_dummy_thumbnails_demo_installer
-cd django_dummy_thumbnails_demo_installer/django-dummy-thumbnails-stable/examples/simple/
+wget -O django_ormex_demo_installer.tar.gz https://github.com/barseghyanartur/django-ormex/archive/stable.tar.gz
+virtualenv django-ormex
+source django-ormex/bin/activate
+mkdir django_ormex_demo_installer/
+tar -xvf django_ormex_demo_installer.tar.gz -C django_ormex_demo_installer
+cd django_ormex_demo_installer/django-ormex-stable/examples/simple/
 pip install -r ../../requirements.txt
-pip install https://github.com/barseghyanartur/django-dummy-thumbnails/archive/stable.tar.gz
+pip install https://github.com/barseghyanartur/django-ormex/archive/stable.tar.gz
 mkdir ../media/
 mkdir ../media/static/
 mkdir ../static/
@@ -15,5 +15,5 @@ mkdir ../tmp/
 cp settings/local_settings.example settings/local_settings.py
 ./manage.py migrate --noinput --traceback -v 3
 ./manage.py collectstatic --noinput --traceback -v 3
-./manage.py dummy_thumbnails_symlink_dummy_images
+./manage.py books_create_test_data --number=100 --traceback -v 3
 ./manage.py runserver 0.0.0.0:8001 --traceback -v 3
