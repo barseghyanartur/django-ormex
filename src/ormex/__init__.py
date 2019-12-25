2f986b4e8ee9c77789b887ffa145aa68c9c221a7
Artur Barseghyan <artur.barseghyan@gmail.com>
2019-12-25 02:58:13 +0100

    Clean up

--- src/ormex/__init__.py
+++ src/ormex/__init__.py
@@ -7,6 +7,4 @@ __version__ = '0.2.1'
 __author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
 __copyright__ = '2017-2020 Artur Barseghyan'
 __license__ = 'GPL-2.0-only OR LGPL-2.1-or-later'
-__all__ = ('default_app_config',)
 
-default_app_config = 'ormex.apps.Config'
